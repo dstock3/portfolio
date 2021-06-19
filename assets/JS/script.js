bccf5caad8696c60e1d60985ae787c86028a299b
David Stockdale <dstock3@gmail.com>
2021-06-19 12:16:15 -0500

    Started navbar. Generating HTML elements using JS. We'll see how this goes....

---
+++ assets/JS/script.js
@@ -0,0 +1,51 @@
+/* CORE FUNCTIONS */
+function elementBuilder (elType, className, parent) {
+    const newElement = document.createElement(elType);
+    newElement.classList.add(className);
+    parent.appendChild(newElement);
+    return newElement;
+};
+
+function linkBuilder(linkArray, ulVar, className, openNewTab) {
+    for (let i = 0; i < linkArray.length; i++) {
+        linkLi = elementBuilder('li', className, ulVar);
+        linkAnchor = elementBuilder('a', 'nav-anchor', linkLi);
+        linkAnchor.textContent = linkArray[i].name;
+        linkAnchor.href = linkArray[i].link;
+        linkLi.setAttribute("id", `${linkArray[i].name.toLowerCase()}`);
+        
+        if (openNewTab === true) {
+            linkAnchor.rel = "noreferrer noopener";
+            linkAnchor.target = "blank";
+        };
+    };
+};
+
+const body = document.getElementsByTagName("body")[0];
+const nav = elementBuilder("nav", "navbar", body);
+const containerDiv = elementBuilder("div", "container", nav)
+nav.classList.add("navbar-expand-lg", "navbar-dark", "menu", "shadow", "fixed-top");
+const navBar = elementBuilder("div", "collapse", containerDiv);
+navBar.classList.add("navbar-collapse", "justify-content-end");
+const ul = elementBuilder("ul", "navbar-nav", navBar);
+
+/* NAV LINKS */
+
+const about = {
+    name: "About",
+    link: "#",
+}
+
+const projects = {
+    name: "Projects",
+    link: "#",
+}
+
+const contact = {
+    name: "Contact",
+    link: "#",
+}
+
+const links = [about, projects, contact];
+
+linkBuilder(links, ul, "nav-links", true);
