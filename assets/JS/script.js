78f1c2aa519afa56d4a0893d07cbc493ff702ad9
David Stockdale <dstock3@gmail.com>
2021-06-28 10:07:50 -0500

    Added HTML for social media links in footer.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -519,6 +519,35 @@ const locationInfo = {
 const contactBoxArray = [phoneInfo, emailInfo, locationInfo];
 contactBoxBuilder(contactBoxArray, footerRow);
 
+const social = elementBuilder("div", "footer-sm", footerRow);
+const socialContainer = elementBuilder("div", "container", social);
+const socialRow = elementBuilder("div", "row", socialContainer);
+socialRow.classList.add("py-4", "text-center", "text-white");
+const socialCol = elementBuilder("div", "col-lg-5", socialRow);
+socialCol.classList.add("col-md-6", "mb-4", "mb-0");
+socialCol.innerHTML = "Connect with Me on Social Media";
+
+const socialIcons = elementBuilder("div", "col-lg-7", socialRow);
+
+const twitterAnchor = elementBuilder("a", "social-media-links", socialIcons);
+twitterAnchor.href = "" //Need to enter my github when complete
+const twitterIcon = elementBuilder("i", "fab", twitterAnchor);
+twitterIcon.classList.add("fa-twitter");
+
+const githubAnchor = elementBuilder("a", "social-media-links", socialIcons);
+githubAnchor.href = "" //Need to enter my github when complete
+const githubIcon = elementBuilder("i", "fab", githubAnchor);
+githubIcon.classList.add("fa-github");
+
+const linkedAnchor = elementBuilder("a", "social-media-links", socialIcons);
+linkedAnchor.href = "" //Need to enter my linkedin when complete
+const linkedIcon = elementBuilder("i", "fab", linkedAnchor);
+linkedIcon.classList.add("fa-linkedin");
+
+
+
+
+
 //BACK TO TOP BUTTON
 
 const topButtonAnchor = elementBuilder("a", "shadow", body);
