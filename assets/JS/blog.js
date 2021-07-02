c6972106cb56d46897f657c7cd0511034aa63724
David Stockdale <dstock3@gmail.com>
2021-07-02 11:00:11 -0500

    Added placeholder image.

--- assets/JS/blog.js
+++ assets/JS/blog.js
@@ -2,7 +2,7 @@
 
 const article = {
     title: "Blog 1 Example Title",
-    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    preview: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam."
 }
 
 //Blog Elements
@@ -29,12 +29,14 @@ blogTitle.innerHTML = article.title;
 const blogBodyBkg = elementBuilder("div", "bg-white", blogCol);
 blogBodyBkg.classList.add ("p-4", "text-start");
 const blogBody = elementBuilder("p", "fw-light", blogBodyBkg);
-blogBody.innerHTML = article.body;
+blogBody.innerHTML = article.preview;
 const blogLink = elementBuilder("a", "float-end", blogCol);
 blogLink.classList.add("me-4");
 blogLink.href = "#"; //Need to insert link once this is built out
 blogLink.innerHTML = "Read More"
 
-const blogCol2 = elementBuilder("div", "col-md-6", blogRow2);
+const blogCol2 = elementBuilder("div", "col-md-5", blogRow2);
+const blogImage = elementBuilder("img", "img-fluid", blogCol2);
+blogImage.src = "https://dummyimage.com/600x400/000/0011ff.png";
 
 
