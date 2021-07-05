d6b4060dad27c9e1fad2a7d7044793a935034825
David Stockdale <dstock3@gmail.com>
2021-07-05 18:22:14 -0500

    Added comments to vid section. Removed comment I used for reference.

--- assets/JS/home/videos.js
+++ assets/JS/home/videos.js
@@ -1,8 +1,11 @@
+//Basic elements
 const vidElements = sectionBuilder("videos");
 const vidSection = vidElements[0];
 const vidContainer = vidElements[1];
 const vidRow = vidElements[2];
 vidRow.classList.add("text-center", "text-white");
+
+//Section Head
 const vidHead = elementBuilder("h1", "display-3", vidRow);
 vidHead.classList.add("fw-bold", "mt-4");
 vidHead.innerHTML = "Videos";
@@ -12,9 +15,12 @@ const vidPara = elementBuilder("p", "lead", vidRow);
 vidPara.classList.add("pt-1");
 vidPara.innerHTML = "lorem ipsum lorem ipsum lorem ipsum";
 
+//Videos
 const vidRow2 = elementBuilder("div", "row", vidContainer);
 vidRow2.classList.add("align-items-center");
 
+
+//Buttons
 const buttonDiv = elementBuilder("div", "text-center", vidRow2);
 const prevButton =  elementBuilder("button", "btn", buttonDiv);
 prevButton.classList.add("btn-outline-light", "fas", "fa-long-arrow-alt-left");
@@ -27,12 +33,3 @@ nextButton.type = "button";
 nextButton.setAttribute("data-bs-target", "#carouselExampleCaptions");
 nextButton.setAttribute("data-bs-slide", "next");
 
-
-
-
-/*
-<button class="btn btn-outline-light fas fa-long-arrow-alt-left" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-</button>
-<button class="btn btn-outline-light fas fa-long-arrow-alt-right" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-</button>
-*/
