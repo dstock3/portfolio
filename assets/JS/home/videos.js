23ccd30e83b681694fc88ff4f28c05bfd41e0d4b
David Stockdale <dstock3@gmail.com>
2021-07-05 12:56:52 -0500

    Added h1 and p elements to video section.

--- assets/JS/home/videos.js
+++ assets/JS/home/videos.js
@@ -3,3 +3,14 @@ const vidSection = vidElements[0];
 const vidContainer = vidElements[1];
 const vidRow = vidElements[2];
 vidRow.classList.add("text-center", "text-white");
+const vidHead = elementBuilder("h1", "display-3", vidRow);
+vidHead.classList.add("fw-bold");
+vidHead.innerHTML = "Videos";
+const vidLine = elementBuilder("hr", "mx-auto", vidRow);
+vidLine.setAttribute("style", "width: 100px; height: 3px; ");
+const vidPara = elementBuilder("p", "lead", vidRow);
+vidPara.classList.add("pt-1");
+vidPara.innerHTML = "lorem ipsum lorem ipsum lorem ipsum";
+
+
+
