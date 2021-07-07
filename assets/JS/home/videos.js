e07654b65610b41272056667fd997a258082d74e
David Stockdale <dstock3@gmail.com>
2021-07-06 20:05:14 -0500

    Adjusted tech section images min width so that they don't get too small on mobile.

--- assets/JS/home/videos.js
+++ assets/JS/home/videos.js
@@ -25,7 +25,7 @@ carouselInnerDiv.setAttribute("id", "carouselExampleCaptions");
 carouselInnerDiv.setAttribute("data-bs-ride", "carousel");
 const carouselActive = elementBuilder("div", "carousel-item", carouselInnerDiv);
 carouselActive.classList.add("active");
-const vidCard = elementBuilder("div", "videos__desc", carouselActive);
+const vidCard = elementBuilder("div", "videos__vid", carouselActive);
 const vidContainer2 = elementBuilder("div", "vid-container", vidCard);
 vidContainer2.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/iJKCj8uAHz8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 const vidDescription = elementBuilder("p", "lh-lg", vidCard);
