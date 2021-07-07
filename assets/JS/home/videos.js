8e6ef349073fa60f984651e6ae0f8fe92d1e6478
David Stockdale <dstock3@gmail.com>
2021-07-06 19:57:48 -0500

    Added video to test out visuals. Need to troubleshoot.

--- assets/JS/home/videos.js
+++ assets/JS/home/videos.js
@@ -25,8 +25,9 @@ carouselInnerDiv.setAttribute("id", "carouselExampleCaptions");
 carouselInnerDiv.setAttribute("data-bs-ride", "carousel");
 const carouselActive = elementBuilder("div", "carousel-item", carouselInnerDiv);
 carouselActive.classList.add("active");
-const vidCard = elementBuilder("div", "videos__card", carouselActive);
+const vidCard = elementBuilder("div", "videos__desc", carouselActive);
 const vidContainer2 = elementBuilder("div", "vid-container", vidCard);
+vidContainer2.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/iJKCj8uAHz8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 const vidDescription = elementBuilder("p", "lh-lg", vidCard);
 vidDescription.innerHTML = `Placeat aut consequatur illo animi optio exercitationem doloribus eligendi iusto atque repudiandae. Distinctio.`
 
