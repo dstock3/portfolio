ced872030506822720b632974ab89a3afeaffd78
David Stockdale <dstock3@gmail.com>
2021-07-08 15:07:31 -0500

    Employed wave border function in blog section.

--- assets/JS/home/blog.js
+++ assets/JS/home/blog.js
@@ -35,14 +35,8 @@ const blogImage = elementBuilder("img", "img-fluid", imageDiv);
 blogImage.src = "https://dummyimage.com/600x400/000/0011ff.png";
 blogImage.classList.add("shadow");
 
-const waveElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-waveElement.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/2000/svg')
-waveElement.setAttributeNS(null, "viewBox", "0 0 1440 320");
-waveElement.classList.add("wave-border");
-const wavePath =  document.createElementNS('http://www.w3.org/2000/svg', 'path');
-wavePath.setAttribute("fill", "#e9ecef");
-wavePath.setAttribute("fill-opacity", "1");
-wavePath.setAttribute("d", "M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,133.3C672,139,768,213,864,202.7C960,192,1056,96,1152,74.7C1248,53,1344,107,1392,133.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z");
-waveElement.appendChild(wavePath);
-blogSection.appendChild(waveElement);
-
+const blogWaveArray = waveGenerator(
+    "M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,133.3C672,139,768,213,864,202.7C960,192,1056,96,1152,74.7C1248,53,1344,107,1392,133.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z",
+    "#e9ecef",
+    blogSection
+);
