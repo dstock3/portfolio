4bd448c9912d203dd9e81b7b8c73b50fd9beebf0
David Stockdale <dstock3@gmail.com>
2021-07-08 14:51:49 -0500

    Added wave border to the bottom of blog section.

--- assets/JS/home/blog.js
+++ assets/JS/home/blog.js
@@ -4,16 +4,6 @@ const blogSection = blogElements[0];
 const blogSectionContainer = blogElements[1];
 const blogRow = blogElements[2];
 
-/*
-const waveElement = elementBuilder("svg", "wave-border", blogSection);
-waveElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-waveElement.setAttribute("viewBox", "0 0 1440 320");
-const wavePath = elementBuilder("path", "wave-path", waveElement);
-wavePath.setAttribute("fill", "#fff");
-wavePath.setAttribute("fill-opacity", "1");
-wavePath.setAttribute("d", "M0,160L48,160C96,160,192,160,288,170.7C384,181,480,203,576,192C672,181,768,139,864,149.3C960,160,1056,224,1152,229.3C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z");
-*/
-
 blogSection.classList.add("gradient");
 blogSectionContainer.classList.add("text-center");
 
@@ -45,6 +35,14 @@ const blogImage = elementBuilder("img", "img-fluid", imageDiv);
 blogImage.src = "https://dummyimage.com/600x400/000/0011ff.png";
 blogImage.classList.add("shadow");
 
-//<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L48,160C96,160,192,160,288,170.7C384,181,480,203,576,192C672,181,768,139,864,149.3C960,160,1056,224,1152,229.3C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-
+const waveElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+waveElement.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/2000/svg')
+waveElement.setAttributeNS(null, "viewBox", "0 0 1440 320");
+waveElement.classList.add("wave-border");
+const wavePath =  document.createElementNS('http://www.w3.org/2000/svg', 'path');
+wavePath.setAttribute("fill", "#e9ecef");
+wavePath.setAttribute("fill-opacity", "1");
+wavePath.setAttribute("d", "M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,133.3C672,139,768,213,864,202.7C960,192,1056,96,1152,74.7C1248,53,1344,107,1392,133.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z");
+waveElement.appendChild(wavePath);
+blogSection.appendChild(waveElement);
 
