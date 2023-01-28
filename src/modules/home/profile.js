7097e875d6acae70d92aabfd131d8dc163c151b1
David Stockdale <dstock3@gmail.com>
2023-01-28 16:37:57 -0600

    adjusted stylistic aspects of profile section.

--- src/modules/home/profile.js
+++ src/modules/home/profile.js
@@ -4,14 +4,15 @@ import { profileData } from '../data.js'
 const profile = () => {
     const profileElements = sectionBuilder("profile");
     const sectionProfile = profileElements[0];
+    sectionProfile.classList.add("text-white", "bg-dark", "py-5", "mt-4", "mb-4");
     const profileContainer = profileElements[1];
     const profileRow = profileElements[2];
     profileRow.classList.add("text-left");
     const profileHead = elementBuilder("h1", "display-2", profileRow);
     profileHead.classList.add("fw-bold", "text-capitalize", "text-center");
-
     profileHead.innerHTML = "Profile";
-
+    const headLine = elementBuilder("div", "profile-line", profileRow);
+    
     const profileArray = Object.keys(profileData).map(key => ({ key, value: profileData[key] }));
 
     for (const { key, value } of profileArray) {
